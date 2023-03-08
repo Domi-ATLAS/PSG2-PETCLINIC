ea430a8983fa40ec4dd1a7808c779719b33a5e1d
josejavier <josalcsan1@alum.us.es>
2023-03-08 12:31:01 +0100

    A.3.2.c

---
+++ src/main/java/org/springframework/samples/petclinic/adptionResponse/AdoptionApplicationController.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.petclinic.adptionResponse;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdoptionApplicationController {
+    
+}
---
+++ src/main/java/org/springframework/samples/petclinic/adptionResponse/AdoptionApplicationRepository.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.petclinic.adptionResponse;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface AdoptionApplicationRepository extends CrudRepository <AdoptionApplication,Integer>{
+    
+}
